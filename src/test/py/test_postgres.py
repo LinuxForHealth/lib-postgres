2aa6d0fc7fa71a9668f0b4f6ecafc62ce2f59b74
Joel Maner <xpander53@yahoo.com>
2021-06-24 12:03:13 -0400

    initial checkin

--- src/test/py/test_postgres.py
+++ src/test/py/test_postgres.py
@@ -1,45 +1,40 @@
 from whpa_cdp_postgres import postgres
+import pytest
 import asyncio
 import asynctest
 from asynctest import CoroutineMock, MagicMock, patch
 
-async def test():
+@pytest.mark.asyncio
+async def test(mocker):
 
-    db = await postgres.create_postgres_pool('Postgres')
+    config = {
+        'username': 'none',
+        'password': 'empty',
+        'hostport': '',
+        'database': ''
+    }
+    db = postgres.Postgres('test',config)
+    db.initialized = True
+    mocker.patch.object(db, "pool")
+    print("START")
+    print('pool='+db.pool)
     result = await db.execute('select * from distributors')
     print(result)
 
-    result = await db.fetch('select * from distributors')
-    print(result)
+    # result = await db.fetch('select * from distributors')
+    # print(result)
 
-    @db.query("select * from distributors")
-    async def test_query(): pass
+    # @db.query("select * from distributors")
+    # async def test_query(): pass
 
-    @db.query("select * from distributors", call='fetch')
-    async def test_fetch_query(): pass
+    # @db.query("select * from distributors", call='fetch')
+    # async def test_fetch_query(): pass
 
-    print(await test_query())
-    print(await test_fetch_query())
+    # print(await test_query())
+    # print(await test_fetch_query())
 
+    #     HL7BatchTrackingDao.insert_new_batch.assert_called_once()
+    # HL7BatchTrackingDao.update_hl7batch_with_storage_id.assert_called_once()
+    # mock_minio_upload.assert_called_once()
+    # mock_kafka_call.assert_called_once()
 
-
-
-class PatientLookupDaoTest(asynctest.TestCase):
-    async def test_patient_lookup(self):
-        '''
-        mock_pool = MagicMock(name='asyncpg')
-        with patch('asyncpg.create_pool', new=mock_pool):
-            pool = MagicMock(name="pool")
-            conn = CoroutineMock(name="conn")
-            mock_pool().__aenter__.return_value = pool
-            pool.acquire().__aenter__.return_value = conn
-            conn.fetch = CoroutineMock()
-            conn.fetch.return_value = 5
-            result = await patient_lookup_dao.patient_lookup(None)
-        
-        self.assertEqual(result, 5)
-        '''
-        pass
-
-if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(test())
